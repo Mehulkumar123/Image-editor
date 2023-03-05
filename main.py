92eec3aaa4484809afe4a4a12d8cc158a413fc55
Mehul Kumar <84561537+Mehulkumar123@users.noreply.github.com>
2023-03-05 13:46:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,70 @@
-import tkinter as tk
-from PIL import Image, ImageTk
-
-class ImageEditor:
-    def __init__(self, image_path):
-        self.root = tk.Tk()
-        self.root.title("Image Editor")
-        self.image_path = image_path
-        self.image = Image.open(image_path)
-        self.photo = ImageTk.PhotoImage(self.image)
-        self.canvas = tk.Canvas(self.root, width=self.image.width, height=self.image.height)
-        self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-        self.canvas.pack()
-        
-        # Add a menu bar with options to adjust brightness and contrast
-        self.menu_bar = tk.Menu(self.root)
-        self.root.config(menu=self.menu_bar)
-        self.edit_menu = tk.Menu(self.menu_bar, tearoff=0)
-        self.menu_bar.add_cascade(label="Edit", menu=self.edit_menu)
-        self.edit_menu.add_command(label="Adjust brightness", command=self.adjust_brightness)
-        self.edit_menu.add_command(label="Adjust contrast", command=self.adjust_contrast)
-        
-        # Add a slider to adjust the speed of the image editor
-        self.speed = 1
-        self.speed_slider = tk.Scale(self.root, from_=1, to=10, orient=tk.HORIZONTAL, label="Speed",
-                                     command=self.set_speed)
-        self.speed_slider.pack()
-        
-        # Add a button to start the game loop
-        self.start_button = tk.Button(self.root, text="Start", command=self.start_game)
-        self.start_button.pack()
-        
-        self.root.mainloop()
-
-    def adjust_brightness(self):
-        # TODO: Implement brightness adjustment
-        pass
-
-    def adjust_contrast(self):
-        # TODO: Implement contrast adjustment
-        pass
-
-    def set_speed(self, value):
-        self.speed = int(value)
-
-    def start_game(self):
-        # Convert speed to an integer if it is a string
-        if isinstance(self.speed, str):
-            self.speed = int(self.speed)
-
-        delay = 10 // self.speed  # Calculate delay based on speed
-        
-        while True:
-            # Rotate the image by 90 degrees
-            self.image = self.image.rotate(90)
-            self.photo = ImageTk.PhotoImage(self.image)
-            self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-            self.canvas.update()
-            self.root.after(delay)
-
-image_editor = ImageEditor("example.jpg")
+from tkinter import *
+from tkinter import filedialog, messagebox
+from PIL import Image, ImageTk, ImageFilter
+class Editor:
+    def __init__(self, master):
+        self.master = master
+        master.title("Image Editor")
+        self.image = None
+        self.photo = None
+        self.canvas = Canvas(master, width=500, height=500)
+        self.canvas.pack(side=TOP, padx=10, pady=10)
+        self.slider_frame = Frame(master)
+        self.slider_frame.pack(side=TOP, padx=10, pady=10)
+        self.blur_slider = self.create_slider(self.slider_frame, "Blur", 0, 20, self.blur_image)
+        self.sharpen_slider = self.create_slider(self.slider_frame, "Sharpen", 0, 20, self.sharpen_image)
+        self.rotate_slider = self.create_slider(self.slider_frame, "Rotate", -180, 180, self.rotate_image)
+        self.open_button = self.create_button(master, "Open", self.open_image)
+        self.save_button = self.create_button(master, "Save", self.save_image)
+    def create_slider(self, parent, label_text, min_value, max_value, command):
+        label = Label(parent, text=label_text)
+        label.pack(side=LEFT, padx=10, pady=10)
+        slider = Scale(parent, from_=min_value, to=max_value, orient=HORIZONTAL, command=command)
+        slider.pack(side=LEFT, padx=10, pady=10)
+        slider.set(0)
+        return slider
+    def create_button(self, parent, button_text, command):
+        button = Button(parent, text=button_text, command=command)
+        button.pack(side=LEFT, padx=10, pady=10)
+        return button
+    def open_image(self):
+        file_path = filedialog.askopenfilename()
+        if file_path:
+            try:
+                self.image = Image.open(file_path)
+                self.image = self.image.resize((500, 500))
+                self.photo = ImageTk.PhotoImage(self.image)
+                self.canvas.create_image(0, 0, anchor=NW, image=self.photo)
+            except Exception as e:
+                messagebox.showerror("Error", f"An error occurred while opening the image: {e}")
+    def save_image(self):
+        try:
+            file_path = filedialog.asksaveasfilename(defaultextension=".png")
+            if file_path:
+                self.image.save(file_path)
+                messagebox.showinfo("Image saved", f"Image saved to {file_path}")
+        except Exception as e:
+            messagebox.showerror("Error", f"An error occurred while saving the image: {e}")
+    def apply_filters(self):
+        blur_value = int(self.blur_slider.get())
+        sharpen_value = int(self.sharpen_slider.get())
+        rotate_value = int(self.rotate_slider.get())
+        if self.image:
+            image_copy = self.image.copy()
+            if blur_value > 0:
+                image_copy = image_copy.filter(ImageFilter.GaussianBlur(radius=blur_value))
+            if sharpen_value > 0:
+                image_copy = image_copy.filter(ImageFilter.UnsharpMask(radius=sharpen_value, percent=150))
+            if rotate_value != 0:
+                image_copy = image_copy.rotate(rotate_value)
+            self.photo = ImageTk.PhotoImage(image_copy)
+            self.canvas.create_image(0, 0, anchor=NW, image=self.photo)
+    def blur_image(self, value):
+        self.apply_filters()
+    def sharpen_image(self, value):
+        self.apply_filters()
+    def rotate_image(self, value):
+        self.apply_filters()
+root = Tk()
+editor = Editor(root)
+root.mainloop()
